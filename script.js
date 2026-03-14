e492d5c5eba5e985fa09af538f2ad95a33a9c395
Luciano Farias <lfarias2022@udec.cl>
2026-03-14 15:43:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Simulator started");
+function simular(){
+
+let aporte = document.getElementById("monthly").value;
+let years = document.getElementById("years").value;
+let bank = document.getElementById("bank").value;
+let market = document.getElementById("market").value;
+
+let months = years * 12;
+
+let ahorro = aporte * months;
+
+let bankCapital = 0;
+let marketCapital = 0;
+
+let bankRate = bank / 100 / 12;
+let marketRate = market / 100 / 12;
+
+for(let i=0; i<months; i++){
+
+bankCapital = bankCapital * (1 + bankRate);
+bankCapital = bankCapital + Number(aporte);
+
+marketCapital = marketCapital * (1 + marketRate);
+marketCapital = marketCapital + Number(aporte);
+
+}
+
+document.getElementById("results").innerHTML =
+`
+Ahorro sin interés: $${Math.round(ahorro)} <br><br>
+Banco: $${Math.round(bankCapital)} <br><br>
+Mercado: $${Math.round(marketCapital)}
+`;
+
+}
